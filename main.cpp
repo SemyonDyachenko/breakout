649ea2b1314dfcd0d9ede7a5359c006d7702c7b9
semyon <semyondyachenko@gmail.com>
2019-06-12 21:34:23 +0300

    игра закончена:физика,звуки,конец игры.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <vector>
 #include "map.h"
 #include <time.h>
@@ -16,6 +17,9 @@ const int winSizeY = 600;
 float speed = 0.3f;
 float ballSpeed = 0.3f;
 const double PI = 3.1415922653;
+int score = 0;
+bool gameover = false;
+
 
 class Ball {
 protected:
@@ -27,13 +31,19 @@ protected:
 
 public:
     bool isCollis;
-    float speed;
+    float speedY;
+    float speedX;
     Ball(sf::RenderWindow * window) {
         this->window = window;
-        this->posX =rand()%window->getSize().x-shape.getRadius();
+        this->posX =rand()%window->getSize().x+radius;
         this->posY = this->window->getSize().y / 2;
         this->radius = 10;
-        this->speed = 0.1f;
+        this->speedY = 0.1f;
+        if(posX > window->getSize().x/2) {
+            this->speedX =-0.15f;
+        } else {
+            this->speedX = 0.15f;
+        }
         this->shape.setPosition(posX,posY);
         this->shape.setRadius(this->radius);
         this->shape.setFillColor(sf::Color(rand()%255,rand()%255,rand()%255));
@@ -42,6 +52,9 @@ public:
     }
 
 
+    int getRadius() {
+     return this->radius;
+    }
 
     sf::FloatRect getRect() {
         return sf::FloatRect(posX, posY, radius, radius);
@@ -53,13 +66,29 @@ public:
     void update(float time,sf::RectangleShape shape)
     {
 
-        if(getRect().intersects(sf::FloatRect(shape.getPosition().x,shape.getPosition().y,shape.getSize().x,shape.getSize().y)))
+        if(getRect().intersects(sf::FloatRect(shape.getPosition().x,shape.getPosition().y-radius,shape.getSize().x,shape.getSize().y)))
         {
-
-          speed = -speed;
+          speedY = -speedY;
         }
-        this->posY +=speed*time;
+        this->posY +=speedY*time;
+        this->posX += speedX * time;
         this->shape.setPosition(posX,posY);
+        if(posX+speedX > window->getSize().x - radius) {
+            speedX = -speedX;
+            if(rand()%(1-10) >= 5) {
+                speedY = -speedY;
+            } else {
+                speedY = speedY;
+            }
+        }
+        if(posX +speed < 3) {
+            speedX =-speedX;
+            if(rand()%(1-10) >= 5) {
+                speedY = speedY;
+            } else {
+                speedY = -speedY;
+            }
+        }
 
 
     }
@@ -74,38 +103,56 @@ public:
 };
 
 
+
+
 int main()
 {
     srand(time(0));
-    //игрок (платформа)
+    // окно
+    RenderWindow window(VideoMode(winSizeX,winSizeY),"Breakout");
+    Texture texture;
+    texture.loadFromFile("../res/block.png");
+    window.setFramerateLimit(65);
+    window.setMouseCursorVisible(false);
+
+    std::cout << "Enter escape for exit"<<std::endl;
+
+   //TODO: игрок
  RectangleShape player;
  player.setSize(Vector2f(100,15));
  player.setPosition(winSizeX/2-player.getSize().x,winSizeY/2+(winSizeY/100*30));
  player.setFillColor(Color::White);
 
 
- //мяч
- /*CircleShape circleBall; //фигуру окружности
- float circleX,circleY; // ее позиции
- circleX = winSizeX/2;
- circleY = winSizeY/2;
- float ballSpeed = 0.15f; // скорость полета мяча
- sf::Color * color = new Color(rand()%255,rand()%255,rand()%255); // цвет мяча
- circleBall.setFillColor(*color); // устанавливаем цвет
-circleBall.setRadius(13);
-circleBall.setPosition(circleX,circleY);
-*/
+ //TODO: картинка gameover-а
+ RectangleShape gameoverShape;
+ Texture gameoverTexture;
+ gameoverTexture.loadFromFile("../res/gameover.jpg");
+ gameoverShape.setTexture(&gameoverTexture);
+ gameoverShape.setSize(sf::Vector2f(window.getSize().x,window.getSize().y));
 
-//return rect
+//TODO: Текст для очков
+Text text;
+sf::Font font;
+font.loadFromFile("../fonts/font.ttf");
+text.setFont(font);
+text.setPosition(10,10);
+text.setCharacterSize(35);
+std::string str = "Score: " + std::to_string(score);
+text.setString(str);
 
 
-
-RenderWindow window(VideoMode(winSizeX,winSizeY),"Breakout");
-Texture texture;
-texture.loadFromFile("../res/block.png");
 RectangleShape rect;
 rect.setSize(Vector2f(40,20));
 rect.setTexture(&texture);
+
+
+SoundBuffer buffer;
+buffer.loadFromFile("../sounds/beep.ogg");
+Sound sound;
+sound.setBuffer(buffer);
+
+
 
 
 Ball ball(&window);
@@ -124,60 +171,78 @@ while(window.isOpen())
             window.close();
     }
 
-    //математика и обновление для мяча
 
 
 
 
+    //UPDATE
     ball.update(time,player);
 
-  /*  if(sf::FloatRect(player.getPosition().x,player.getPosition().y,player.getSize().x,player.getSize().y).intersects(sf::FloatRect(circleBall.getPosition().x,circleBall.getPosition().y+circleBall.getRadius(),circleBall.getRadius(),circleBall.getRadius()))) {
-        circleY += player.getPosition().y;
-        circleY += ballSpeed*time;
-        circleBall.setPosition(circleX,circleY);
-    } else {
-       circleY += ballSpeed * time;
-       circleBall.setPosition(circleX,circleY);
-    }
-*/
-    //TODO: сдесь ошибка в том что нужно прибовлять к позиции speed * time  а не присваивать
+
 
 
     // --------------------------
 
-    //control
-    if(Keyboard::isKeyPressed(Keyboard::A)) {
-         player.move(-speed*time,0);
-    } else
-        if (Keyboard::isKeyPressed(Keyboard::D)) {
-            player.move(speed*time,0);
+    //управление движеним платформы
+    if(!gameover) {
+        if (Keyboard::isKeyPressed(Keyboard::A)) {
+            player.move(-speed * time, 0);
+        } else if (Keyboard::isKeyPressed(Keyboard::D)) {
+            player.move(speed * time, 0);
+        }
     }
 
+   //очистка экрана
+    window.clear(sf::Color::Black);
 
-    window.clear();
+
 
     for (int i = 0; i < HEIGHT_MAP; i++)
         for (int j = 0; j < WIDTH_MAP; j++)
         {
 
+
+
             if(TileMap[i][j] == '0') {
                 rect.setPosition(j * 40, i * 20);
-                if(ball.getRect().intersects(sf::FloatRect(rect.getPosition().x,rect.getPosition().y,rect.getSize().x,rect.getSize().y))) {
+
+                if(ball.getRect().intersects(sf::FloatRect(rect.getPosition().x,rect.getPosition().y,rect.getSize().x,rect.getSize().y)))
+                {
                     TileMap[i][j] = ' ';
                     ball.isCollis = true;
-                    ball.speed = -ball.speed;
+                    ball.speedY = -ball.speedY;
+                    if(rand()%(1-10)>= 5) {
+                        ball.speedX = -ball.speedX;
+                    } else {
+                        ball.speedX = ball.speedX;
+                    }
+                    score++;
+                    std::string str = "Score: " + std::to_string(score);
+                    text.setString(str);
+                    sound.play();
                 }
-
+            if(!gameover) {
                 window.draw(rect);
+            }
 
             }
 
-    }
+            if(ball.getRect().top + ball.getRadius() > window.getSize().y) {
+                gameover =true;
+                window.draw(gameoverShape);
+                window.draw(text);
+            }
 
-    window.draw(player);
-  //  window.draw(circleBall);
-     ball.render();
-    window.display();
+
+
+
+
+    }
+    if(!gameover) {
+        window.draw(player);//рисуем игрока
+        ball.render();//рисуем мячик
+    }
+     window.display(); //отрисовываем экран
 }
 
 return 0;
